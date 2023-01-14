c13b905fa3cbe7cfcf9bf87314512485e84460fc
mamezlf <mamezhlf@gmail.com>
2023-01-14 22:03:31 +0900

    some new videos uploaded

--- Cpro/tsload.c
+++ Cpro/tsload.c
@@ -46,16 +46,38 @@ int main(void)
 
   FILE *fp;
   int num;
-  num=FindSongNum(fp);
+  num = FindSongNum(fp);
+
+  char href[SIZE];
+  char date[SIZE];
+
+  char iframe[SIZE];
+
+  printf("youtube link: ");
+  scanf("%s", &href);
+  printf("date: ");
+  scanf("%s", &date);
+  fprintf(out, "<h4> <a href=\"%s\" title= \"RPD%s\"\n>",href,date);
+
+  printf("iframe: ");
+  fflush(stdin);
+  scanf("%[^\n]", &iframe);
+
+  
+  fprintf(out,"<!--title here--></a></h4>\n%s", iframe);
+  fprintf(out, "\n<div class=\"tslist\" id=\"RPD%s>\"", date);
+  fprintf(out, "<ul style=\"list-style: none\">");
 
   char buf[256];
-  for ( i = 0; i < num&&fgets(buf, sizeof(buf), in); i++)
+  for (i = 0; i < num && fgets(buf, sizeof(buf), in); i++)
   {
-    fprintf(out,"<li>%s</li>\n",buf);
+    fprintf(out, "<li>%s</li>\n", buf);
   }
-  
+
+  fprintf(out, "<br />\n</ul>\n</div>");
+
   fclose(in);
   fclose(out);
-  
+
   return 0;
 }
